04d77cab821a8beff08f1986df78c947615af5de
Noam Nachshon <noam1126@gmail.com>
2024-03-05 11:16:44 +0200

    update

---
+++ app/src/main/java/com/example/myapplication/fragments/fragmentSettings.java
@@ -0,0 +1,83 @@
+package com.example.myapplication.fragments;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+import androidx.navigation.Navigation;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.CompoundButton;
+import android.widget.Switch;
+
+import com.example.myapplication.R;
+
+/**
+ * A simple {@link Fragment} subclass.
+ * Use the {@link fragmentSettings#newInstance} factory method to
+ * create an instance of this fragment.
+ */
+public class fragmentSettings extends Fragment {
+
+    // TODO: Rename parameter arguments, choose names that match
+    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+    private static final String ARG_PARAM1 = "param1";
+    private static final String ARG_PARAM2 = "param2";
+
+    // TODO: Rename and change types of parameters
+    private String mParam1;
+    private String mParam2;
+
+    private Switch swithDarkMode;
+
+    public fragmentSettings() {
+        // Required empty public constructor
+    }
+
+    /**
+     * Use this factory method to create a new instance of
+     * this fragment using the provided parameters.
+     *
+     * @param param1 Parameter 1.
+     * @param param2 Parameter 2.
+     * @return A new instance of fragment fragmentSettings.
+     */
+    // TODO: Rename and change types and number of parameters
+    public static fragmentSettings newInstance(String param1, String param2) {
+        fragmentSettings fragment = new fragmentSettings();
+        Bundle args = new Bundle();
+        args.putString(ARG_PARAM1, param1);
+        args.putString(ARG_PARAM2, param2);
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        if (getArguments() != null) {
+            mParam1 = getArguments().getString(ARG_PARAM1);
+            mParam2 = getArguments().getString(ARG_PARAM2);
+        }
+
+        swithDarkMode=(Switch) getView().findViewById(R.id.swithDarkMode);
+        swithDarkMode.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                // do something, the isChecked will be
+                // true if the switch is in the On position
+            }
+        });
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View view = inflater.inflate(R.layout.fragment_settings, container, false);
+        return view;
+    }
+
+
+}
